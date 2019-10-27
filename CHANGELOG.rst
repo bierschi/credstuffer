92b3f59e701c6f8d796b78b97144dde40028c62a
Christian Bierschneider <christian.bierschneider@web.de>
2019-10-27 16:11:49 +0100

    added first init files

---
+++ CHANGELOG.rst
@@ -0,0 +1,1 @@
+- 27.10.2019: development start
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from credstuffer import __version__, __author__, __email__, __license__
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", encoding='utf-8') as f:
+    readme = f.read()
+
+with open("CHANGELOG.rst") as f:
+    changelog = f.read()
+
+
+setup(
+    name="credstuffer",
+    version=__version__,
+    description="credentials stuffing",
+    long_description=readme + "\n\n" + changelog,
+    license=__license__,
+    author=__author__,
+    author_email=__email__,
+    url="https://github.com/bierschi/credstuffer",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=required,
+    keywords=["credstuffer", "credentials", "stuffing", "hacking"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    entry_points={
+        "console_scripts": [
+            'credstuffer = '
+        ],
+    },
+    zip_safe=False,
+)
